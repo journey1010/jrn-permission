27f08f25016263f045b3aa6dec518c06b36c8873
journii gorle <ginopaflo001608@gmail.com>
2025-03-19 15:19:58 -0500

    fix: user interface for permissions

--- src/Contracts/User.php
+++ src/Contracts/User.php
@@ -39,13 +39,8 @@ interface User {
     /**
      *  @return true if role added successfully or false if role already exists  for this user 
     */
-    public static function addRole(int $user, string|int $role, ?string $userType = null): bool;
-    
-    /**
-     * Add role for user and excludes any role that already been added to the user
-     */
-    public static function addRoles(int $user, array $roles): bool;
-    
+    public static function addRole(int $user, string|BackedEnum|array $role, ?string $userType = null): bool;
+      
     /**
      * All roles for current user will be replace by the array given
     */
@@ -59,12 +54,7 @@ interface User {
     /**
      *  @return true if permission added successfully or false if role already exists  for this user 
     */
-    public static function addPermission(int $user, string|array|BackedEnum $permission, ?string $userType = null): bool;
-
-    /**
-     * Add permissions for user and excludes any permission that already been added to the user
-    */
-    public static function addPermissions(int $user, array $permissions): bool;
+    public static function addPermission(int $user, string|BackedEnum|array $permission, ?string $userType = null): bool;
 
     /**
      * All permission for current user will be replace by the array given
