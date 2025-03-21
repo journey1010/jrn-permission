8064d23b866677d6eb24d193466830c819e9e780
Journey PC white <ginopaflo001608@gmail.com>
2025-03-21 00:40:47 -0500

    feat: init role class

--- src/Rbac/Role.php
+++ src/Rbac/Role.php
@@ -9,20 +9,28 @@ use Illuminate\Support\Facades\Config;
 
 class Role implements RoleContract{
 
-    protected static $table = Config('jrnRbac.tables.roles'); 
-        
-    public static function permission(string|int $role): Collection
+    protected static $table; 
+    
+    private static function init(): void
     {
+        if(!isset(static::$table)){
+            static::$table = Config('jrnRbac.tables.roles');
+        }
+    }
+
+    public static function permission(string|int $role): Collection
+    {static::init();
         return new Collection(); 
     }
 
     public static function find(string|int $role): int
-    {
+    {static::init();
         return 1; 
     }
 
     public static function findOrCreate(string $name): int
     { 
+        static::init();
         $role = DB::table(static::$table)->where('name', str_replace(' ', '', strtolower($name)))->first();
         if(is_null($role)){
             
